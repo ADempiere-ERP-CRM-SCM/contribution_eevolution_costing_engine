9662f398e72e4a4caa96c373e57b8d2ecc3cbf2c
Víctor Pérez <victor.perez@e-evolution.com>
2010-09-10 19:13:48 +0000

    fix script migration

--- extension/costengine/src/main/resources/script/postgresql/019_AddProcessing.sql
+++ extension/costengine/src/main/resources/script/postgresql/019_AddProcessing.sql
@@ -8,9 +8,9 @@ INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Refe
 INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=59789 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
 ;
 
--- Sep 9, 2010 10:40:10 PM CDT
+-- Sep 7, 2010 6:51:11 PM CDT
 -- Cost Engine
-INSERT INTO t_alter_column values('m_costdetail','Processing','CHAR(1)',null,'N')
+ALTER TABLE M_CostDetail ADD COLUMN Processing CHAR(1) DEFAULT 'N' CHECK (IsReversal IN ('Y','N'))
 ;
 
 -- Sep 9, 2010 10:50:07 PM CDT
