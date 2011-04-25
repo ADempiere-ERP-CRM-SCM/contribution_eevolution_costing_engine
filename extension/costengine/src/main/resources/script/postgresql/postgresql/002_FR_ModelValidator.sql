b70bbc4c19288e0c7418c48b56e7f1c6012fae3b
Víctor Pérez <victor.perez@e-evolution.com>
2011-04-25 17:56:08 +0000

    Red1, please be careful and more orderly with commits this is a branch for COSTING ENGINE and not for fixed assets

--- extension/costengine/src/main/resources/script/postgresql/postgresql/002_FR_ModelValidator.sql
+++
@@ -1,5 +0,0 @@
--- Jun 19, 2010 2:02:07 PM CDT
--- Model Validator Fixed Asset
-INSERT INTO AD_ModelValidator (AD_Client_ID,AD_ModelValidator_ID,AD_Org_ID,Created,CreatedBy,Description,EntityType,Help,IsActive,ModelValidationClass,Name,SeqNo,Updated,UpdatedBy) VALUES (0,50004,0,TO_TIMESTAMP('2010-06-19 14:02:00','YYYY-MM-DD HH24:MI:SS'),0,'Fixed Assets','ARH1','Fixed Assets','Y','ro.arhipac.adempiere.fa.ModelValidator','Model Validator to Fixed Assets',70,TO_TIMESTAMP('2010-06-19 14:02:00','YYYY-MM-DD HH24:MI:SS'),0)
-;
-
